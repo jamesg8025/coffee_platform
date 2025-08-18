9167436733f6beb16a151c1cf12da7ce2b3132a9
James Gonzalez <jamesg8025@gmail.com>
2025-08-18 03:18:42 -0500

    Setup Spring Boot project

---
+++ backend/src/main/java/com/coffeeplatform/CoffeePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeePlatformApplication.class, args);
+    }
+
+}
